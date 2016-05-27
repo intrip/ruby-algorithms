0b17eb1aa8315d9a431d4d6f3841ac660201e75c
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2016-05-27 09:20:24 +0200

    fixed quick_sort

--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -16,68 +16,38 @@ def partition(arr,start,to)
 	# lower part
 	li = start
 
-	#higher part
-	for ui in li..to
+	for ui in li..(to-1)
 		if arr[ui] <= pivot
 			arr[li],arr[ui]=arr[ui],arr[li]
 			li+=1
 		end
 	end
 
-	p "LI: ", li, "TO: ", to
 	# move the pivot in middle
-	arr[li+1],arr[to]=arr[to],arr[li+1]
-
-	li+1
+	arr[li],arr[to]=arr[to],arr[li]
+	li
 end
 
 
 def quick_sort(arr,start=0, to=nil)
   to = arr.length() -1 if to.nil?
 
-  return unless start < to
+  return if start >= to
 
   partition_index = partition(arr,start,to)
-  p partition_index, start, to; sleep 1 
-  p arr
   # we exclude the pi_item because is already sorted: the pivot
   quick_sort(arr,start,partition_index-1)
   quick_sort(arr,partition_index+1,to)
-
+  arr
 end
-
-
-# def quick_sort(arr,start=0, to=nil)
-#   to = arr.length() -1 if to.nil?
-
-#   return if start > to
-
-#   # pickup the pivot el
-#   pivot = arr[to] 
-
-#   l=[]
-#   r=[]
-
-#   for i in start..(to-1)
-#   	if arr[i] < pivot
-#   		l << arr[i]
-#   	else
-#   		r << arr[i]
-#   	end
-#   end
-
-#   quick_sort(l)
-#   quick_sort(r)
-
-#   l + [pivot] + r
-# end
 
 arr = [3, 4, 5, 1, 2]
 
 times = 10 ** 6
 require 'benchmark'
-# Benchmark.bm do |x|
-# 	x.report("quick_sort: ") { for i in 1..times; quick_sort(arr.clone) end;}
-# end
+Benchmark.bm do |x|
+	x.report("quick_sort: ") { for i in 1..times; quick_sort(arr.clone) end;}
+end
+
 puts "arr #{arr}"
 puts "Quick sort: #{quick_sort(arr.clone)}"
