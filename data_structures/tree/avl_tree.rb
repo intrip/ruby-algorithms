15c9757fe3cb524242e3bb1fa56711819d26bce3
Jacopo <beschi.jacopo@gmail.com>
2019-09-11 10:38:55 +0200

    AVL tree with insert

--- data_structures/tree/avl_tree.rb
+++ data_structures/tree/avl_tree.rb
@@ -1,276 +1,201 @@
-require_relative '../utils/print_binary_tree.rb'
 require 'byebug'
+require 'forwardable'
 
 class Node
-  KEY_SPAN = 3
+  UNDEFINED = Object.new
 
-  attr_reader :key
-  attr_accessor :p, :l, :r, :b, :height
+  attr_reader :key, :val, :height
+  attr_accessor :left, :right
 
-  def initialize(key:, p: nil, l: nil, r: nil, b: 0, height: 0)
+  def initialize(key, val)
     @key = key
-    @p = p
-    @l = l
-    @r = r
-    @b = b
-    @height = height
+    @val = val
+    @height = 1
+    @left = @right = EMPTY
   end
 
-  def render(padding)
-    print pad(*padding, "#{key}_#{b}@#{height}")
+  def dump
+    has_childrens = !left.empty? || !right.empty?
+    res = key.to_s
+    if has_childrens
+      res += "("
+      res += "#{left.dump}," unless left.empty?
+      res += "#{right.dump}" unless right.empty?
+      res += ")"
+    end
+    res
   end
 
-  def ==(other = nil)
-    return false unless other
-
-    key == other.key
+  def empty?
+    false
   end
 
-  private
+  def insert(new_key, val)
+    case new_key <=> key
+    when -1
+      @left = left.insert(new_key, val)
+    when 1
+      @right = right.insert(new_key, val)
+    when 0
+      @val = val
+    else
+      raise "Cannot compare #{new_key} and #{key} using <=>"
+    end
+    rotate
+  end
 
-  def pad(lpad, rpad, str)
-    lpad + str + rpad
+  def get(key)
+    if key == @key
+      val
+    elsif key > @key
+      right.get(key)
+    else
+      left.get(key)
+    end
+  end
+
+  def validate!
+    if (left.height - right.height).abs > 1
+      raise "Node #{key} is unbalanced: #{left.height} - #{height} - #{right.height}"
+    end
+
+    if [left.height, right.height].max + 1 != height
+      raise "Failed to update height for #{key}: #{left.height} - #{height} - #{right.height}"
+    end
+
+    left.validate! unless left .empty?
+    right.validate! unless right.empty?
+  end
+
+  protected
+
+  def rotate
+    case left.height - right.height
+    when 2
+      if left.left.height < left.right.height
+        @left = left.rotate_left
+      end
+      root = rotate_right
+    when -2
+      if right.right.height < right.left.height
+        @right = right.rotate_right
+      end
+      root = rotate_left
+    else
+      root = self
+    end
+
+    root.update_height
+    root
+  end
+
+  def update_height
+    @height = if right.height > left.height
+      right.height
+    else
+      left.height
+    end + 1
+  end
+
+  # Rotates a node left
+  #
+  # For example here we rotate left 'a':
+  #
+  #       a                  b
+  #      / \                / \
+  #     c   b     =>       a   f
+  #        / \            / \
+  #       e   f          c   e
+  #
+  def rotate_left
+    root = right
+    @right = right.left
+    root.left = self
+    root.left.update_height
+    root
+  end
+
+  # Rotates a node right
+  #
+  # For example here we rotate right 'b':
+  #
+  #       b               a
+  #      / \             / \
+  #     a   f     =>    c   b
+  #    / \                 / \
+  #   c   e               e   f
+  #
+  def rotate_right
+    root = left
+    @left = left.right
+    root.right = self
+    root.right.update_height
+    root
   end
 end
 
 class EmptyNode < Node
-  class << self
-    def from_node(node, nil_node)
-      self.new(key: nil, p: node, height: node.height + 1)
-    end
+  def initialize
+    @height = 0
   end
 
-  def render(padding)
-    print pad(*padding, ' * ')
+  def insert(key, val)
+    Node.new(key, val)
+  end
+
+  def dump
+    ''
+  end
+
+  def get(key)
+    UNDEFINED
+  end
+
+  def empty?
+    true
+  end
+
+  def rotate
+    self
   end
 end
+EMPTY = EmptyNode.new.freeze
 
 class AVLTree
-  attr_reader :max_height, :root, :debug
+  extend Forwardable
 
-  def initialize(root = nil, debug = false)
-    @root = root
-    @max_height = 0
-    @debug = debug
+  attr_reader :root, :default_value
+
+  def_delegators :@root, :dump, :validate!
+
+  def initialize(default_value = nil)
+    @root = EmptyNode.new
+    @default_value = default_value
   end
 
-  def insert(key)
-    y = nil
-    x = root
+  def insert(key, val)
+    @root = @root.insert(key, val)
+  end
 
-    while !x.nil?
-      y = x
-      if key < x.key
-        x.b -= 1
-        x = x.l
-      else
-        x.b += 1
-        x = x.r
-      end
-    end
-
-    if y.nil?
-      @root = z = Node.new(key: key)
+  def get(key)
+    res = root.get(key)
+    if res == Node::UNDEFINED
+      default_value
     else
-      z = Node.new(key: key, p: y, height: y.height + 1)
-      if z.key < y.key
-        y.l = z
-      else
-        y.r = z
-      end
-    end
-
-    # set tree max height
-    if z.height > max_height
-      @max_height = z.height
-    end
-
-    avl_fixup(z)
-
-    z
-  end
-
-  def horizontal_tree_walk
-    PrintBinaryTree.new(root, max_height, nil, EmptyNode, ->(node) { node.nil? }).render
-  end
-
-  # rotates a node left
-  #
-  # for example here we
-  # rotate left 'a':
-  #
-  #       a
-  #      / \
-  #     c   b
-  #        / \
-  #       e   f
-  #
-  #     =>
-  #
-  #       b
-  #      / \
-  #     a   f
-  #    / \
-  #   c   e
-  #
-  def rotate_l(z)
-    y = z.r
-    return false unless y
-
-    puts_debug "Rotate L: #{z.key}"
-
-    y_new_height = z.height
-    # reduce tree_height to force recaulculation if rotation could reduce the tree_height
-    # if z.height == (max_height - 2)
-    #   @max_height = z.height
-    # end
-
-    # set parent of y
-    y.p = z.p
-    if z == root
-      @root = y
-    elsif z == z.p.l
-      z.p.l = y
-    else
-      z.p.r = y
-    end
-
-    # set z.r to y.l
-    z.r = y.l
-    y.l.p = z if !y.l.nil?
-
-    # set y.l
-    y.l = z
-    z.p = y
-
-    update_height(y, y_new_height)
-
-    z
-  end
-
-  # rotates a node right
-  #
-  # for example here we
-  # rotate right 'b':
-  #
-  #       b
-  #      / \
-  #     a   f
-  #    / \
-  #   c   e
-  #
-  #     =>
-  #
-  #       a
-  #      / \
-  #     c   b
-  #        / \
-  #       e   f
-  #
-  def rotate_r(z)
-    y = z.l
-    return false unless y
-
-    puts_debug "Rotate R: #{z.key}"
-
-    y_new_height = z.height
-    # reduce tree_height to force recaulculation if rotation could reduce the tree_height
-    # if z.height == (max_height - 2)
-    #   @max_height = z.height
-    # end
-
-    # set parent of y
-    y.p = z.p
-    if z == root
-      @root = y
-    elsif z.p.l == z
-      z.p.l = y
-    else
-      z.p.r = y
-    end
-
-    # set z.l to y.r
-    z.l = y.r
-    y.r.p = z if y.r
-
-    # set y.r
-    y.r = z
-    z.p = y
-
-    update_height(y, y_new_height)
-
-    z
-  end
-
-  def search(key)
-    current = root
-
-    while current && current.key != key
-      current = if key < current.key
-        current.l
-      else
-        current.r
-      end
-    end
-
-    current
-  end
-
-  private
-
-  def update_height(node, height)
-    node.height = height
-    if node.l
-      update_height(node.l, height + 1)
-    end
-    if node.r
-      update_height(node.r, height + 1)
+      res
     end
   end
-
-  def avl_fixup(z)
-    grandparent = z.p&.p
-    return if !grandparent
-    return if grandparent.b < 2 && grandparent.b > -2
-
-    parent = z.p
-
-    if parent == grandparent.l
-      if z == parent.r
-        # makes z parent.l
-        z.p = parent.p
-        grandparent.l = z
-        z.l = parent
-        parent.p = z
-        parent.r = nil
-
-        z = parent
-        parent = z.p
-        update_height(parent, z.height)
-      end
-
-      # TODO fix balance values here but also on rotations at first
-      # also reduce tree max height on rotation (fix commented code)
-      rotate_r(grandparent)
-    else
-      # z needs to be parent.r
-
-    end
-  end
-
-  def puts_debug(txt)
-    puts txt if debug
-  end
+  alias :[] :get
 end
 
-avl = AVLTree.new(nil, true)
-[38, 19, 41, 12, 13].each do |i|
-  avl.insert(i)
-  avl.horizontal_tree_walk
+avl = AVLTree.new
+[[38,1], [19,2], [41,3], [12,4], [13,5], [20,6], [21,7]].each do |k,v|
+  avl.insert(k, v)
+  puts avl.dump
+  avl.validate!
   readline
 end
 
-# avl.rotate_r(avl.search(19))
-# TODO
-# implement the balance and rotation
-# recalculate balance and height after rotation too
+p avl[99]
+# Add doc for rotate
+# Implement delete and the tree walks
