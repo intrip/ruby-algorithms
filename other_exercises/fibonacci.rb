765f5d40e8a7c2543bbd6c090f7239d7edeb6421
Jacopo <beschi.jacopo@gmail.com>
2020-01-10 22:30:14 +0100

    Add iterative fibonacci

--- other_exercises/fibonacci.rb
+++ other_exercises/fibonacci.rb
@@ -1,18 +1,37 @@
 require 'benchmark'
 
-# enable tail call optimization
+# Enable tail call optimization
 RubyVM::InstructionSequence.compile_option = {
   tailcall_optimization: true,
   trace_instruction: false
 }
 
-# This method can also be speeded up with dynamic programming
-# this function is tail recursive because always returns a function or a value
+# This function is tail recursive because always returns a function or a value
+# Time complexity: O(n).
 def fib(n, a = 0, b = 1)
   return a if n == 0
   return b if n == 1 && a == 0
 
   fib(n - 1, b, a + b)
+end
+
+# This is the iterative version of fib using DP.
+# The iterative version is faster than the recursive one
+# due to the overhead of function calls.
+# Time complexity: O(n).
+def fib_iter(n)
+  return n if n <= 1
+
+  a = 0
+  b = 1
+
+  2.upto(n) do |i|
+    tmp = b
+    b = a + b
+    a = tmp
+  end
+
+  b
 end
 
 # This approximation allows to calculate fib in O(1) using an approximation
@@ -25,6 +44,7 @@ def quick_fib(n)
 end
 
 Benchmark.bm do |x|
-  x.report("fib recursive: ") { puts fib(100) }
-  x.report("fib formula: ") { puts quick_fib(100) }
+  x.report("fib recursive: ") { puts fib(500) }
+  x.report("fib iter: ") { puts fib_iter(500) }
+  x.report("fib formula: ") { puts quick_fib(500) }
 end
